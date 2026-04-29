c5d2f74f22263d89ec9ecc8a24ff191ff77f5570
architraj99 <rajarchit99@gmail.com>
2026-04-29 17:47:01 +0530

    Added popup step navigation logic
    Added step by step functionality to the intro popup
    
    user can now click next to move through different instructions
    
    updated popup title and text dynamically for each step
    
    final step changes button to start and closes the popup

---
+++ script.js
@@ -0,0 +1,30 @@
+let step = 0;
+
+function nextStep() {
+
+    let title = document.getElementById("popupTitle");
+    let text = document.getElementById("popupText");
+    let button = document.querySelector("#popup button");
+
+    step++;
+
+    if (step === 1) {
+        title.innerText = "How To Play";
+        text.innerText = "Flip two cards and try to find matching pairs";
+    }
+
+    else if (step === 2) {
+        title.innerText = "Game Rules";
+        text.innerText = "If cards match they stay open, otherwise they flip back";
+    }
+
+    else if (step === 3) {
+        title.innerText = "Levels";
+        text.innerText = "Game has multiple levels with increasing difficulty";
+        button.innerText = "Start Game";
+    }
+
+    else {
+        document.getElementById("popup").style.display = "none";
+    }
+}
